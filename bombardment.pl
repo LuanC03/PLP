e56e2e140cc0bfa65705d6b69aaf9e5b64349f9f
LUAN CARLOS DA SILVA BEZERRA 118110602 <luancsb@lcc2-34.lcc.ufcg.edu.br>
2018-07-16 17:14:07 -0300

    Atualizando Projeto

--- bombardment.pl
+++ bombardment.pl
@@ -1,33 +1,6 @@
+/*javafree.uol.com.br/topic-852645-Prolog-jogo-da-velha.html
+*/
 :- initialization (main).
-
-posicao(a, 1, virus(X)). posicao(a, 2, virus(X)). posicao(a, 3, virus(X)). posicao(a, 4, virus(X)). posicao(a, 5, virus(X)). posicao(a, 6, virus(X)). posicao(a, 7, virus(X)). posicao(a, 8, virus(X)). posicao(a, 9, virus(X)). posicao(a, 10, virus(X)). posicao(a, 11, virus(X)). posicao(a, 12, virus(X)). 
-
-posicao(b, 1, virus(X)). posicao(b, 2, virus(X)). posicao(b, 3, virus(X)). posicao(b, 4, virus(X)). posicao(b, 5, virus(X)). posicao(b, 6, virus(X)). posicao(b, 7, X). posicao(b, 8, virus(X)). posicao(b, 9, virus(X)). posicao(b, 10, virus(X)). posicao(b, 11, virus(X)). posicao(b, 12, virus(X)).
- 
-posicao(c, 1, virus(X)). posicao(c, 2, virus(X)). posicao(c, 3, virus(X)). posicao(c, 4, virus(X)). posicao(c, 5, virus(X)). posicao(c, 6, virus(X)). posicao(c, 7, virus(X)). posicao(c, 8, virus(X)). posicao(c, 9, virus(X)). posicao(c, 10, virus(X)). posicao(c, 11, virus(X)). posicao(c, 12, virus(X)). 
-
-posicao(d, 1, virus(X)). posicao(d, 2, virus(X)). posicao(d, 3, virus(X)).posicao(d, 4, virus(X)). posicao(d, 5, virus(X)). posicao(d, 6, Xvirus(X)). posicao(d, 7, virus(X)). posicao(d, 8, virus(X)). posicao(d, 9, virus(X)). posicao(d, 10, virus(X)). posicao(d, 11, virus(X)). posicao(d, 12, virus(X)). 
-
-posicao(e, 1, virus(X)). posicao(e, 2, X). posicao(e, 3, virus(X)). posicao(e, 4, virus(X)). posicao(e, 5, X). posicao(e, 6, virus(X)). posicao(e, 7, virus(X)). posicao(e, 8, virus(X)). posicao(e, 9, virus(X)). posicao(e, 10, virus(X)). posicao(e, 11, virus(X)). posicao(e, 12, virus(X)). 
-
-posicao(f, 1, virus(X)). posicao(f, 2, virus(X)). posicao(f, 3, virus(X)). posicao(f, 4, virus(X)). posicao(f, 5, virus(X)). posicao(f, 6, virus(X)). posicao(f, 7, virus(X)). posicao(f, 8, virus(X)). posicao(f, 9, virus(X)). posicao(f, 10, virus(X)). posicao(f, 11, virus(X)). posicao(f, 12, virus(X)). 
-
-posicao(g, 1, virus(X)). posicao(g, 2, virus(X)). posicao(g, 3, virus(X)). posicao(g, 4, virus(X)). posicao(g, 5, virus(X)). posicao(g, 6, virus(X)). posicao(g, 7, virus(X)). posicao(g, 8, virus(X)). posicao(g, 9, virus(X)). posicao(g, 10, virus(X)). posicao(g, 11, virus(X)). posicao(g, 12, virus(X)). 
-
-posicao(h, 1, virus(X)). posicao(h, 2, virus(X)). posicao(h, 3, virus(X)). posicao(h, 4, virus(X)). posicao(h, 5, virus(X)). posicao(h, 6, virus(X)). posicao(h, 7, virus(X)). posicao(h, 8, virus(X)). posicao(h, 9, virus(X)). posicao(h, 10, virus(X)). posicao(h, 11, virus(X)). posicao(h, 12, virus(X)). 
-
-posicao(i, 1, virus(X)). posicao(i, 2, virus(X)). posicao(i, 3, virus(X)). posicao(i, 4, virus(X)). posicao(i, 5, virus(X)). posicao(i, 6, virus(X)). posicao(i, 7, virus(X)). posicao(i, 8, virus(X)). posicao(i, 9, virus(X)). posicao(i, 10, virus(X)). posicao(i, 11, virus(X)). posicao(i, 12, virus(X)). 
-
-posicao(j, 1, virus(X)). posicao(j, 2, virus(X)). posicao(j, 3, virus(X)). posicao(j, 4, virus(X)). posicao(j, 5, virus(X)). posicao(j, 6, virus(X)). posicao(j, 7, virus(X)). posicao(j, 8, virus(X)). posicao(j, 9, virus(X)). posicao(j, 10, virus(X)). posicao(j, 11, virus(X)). posicao(j, 12, virus(X)). 
-
-posicao(l, 1, virus(X)). posicao(l, 2, virus(X)). posicao(l, 3, virus(X)). posicao(l, 4, virus(X)). posicao(l, 5, virus(X)). posicao(l, 6, virus(X)). posicao(l, 7, virus(X)). posicao(l, 8, virus(X)). posicao(l, 9, virus(X)). posicao(l, 10, virus(X)). posicao(l, 11, virus(X)). posicao(l, 12, virus(X)). 
-
-posicao(m, 1, virus(X)). posicao(m, 2, virus(X)). posicao(m, 3, virus(X)). posicao(m, 4, virus(X)). posicao(m, 5, virus(X)). posicao(m, 6, virus(X)). posicao(m, 7, virus(X)). posicao(m, 8, virus(X)). posicao(m, 9, virus(X)). posicao(m, 10, virus(X)). posicao(m, 11, virus(X)). posicao(m, 12, virus(X)). 
- 
-virus(X, 1) :- X is 1.
-virus(X, 2) :- X is 2.
-virus(X, 3) :- X is 3.
-virus(X, 4) :- X is 4.
 
 main :-
 	nl, write('      ###########      bombardment of the virus      ###########'),nl,
@@ -50,10 +23,11 @@ resumo :-
 
 
 iniciar	 :-
-	nl,writeln('    Bem Vindo ao ano começo do fim , a gasolina está acabando e o mundo também'),
+	nl,writeln('------------------------------------------------------------------------------'),nl,
+	writeln('    Bem Vindo ao ano começo do fim , a gasolina está acabando e o mundo também'),
 	writeln('só você poderá salva-lo e deter o virus mortal! Baseado em uma historia real.'),
 	writeln('Se voce nao lembra foi por que o mundo ja foi salvo e sua linha do tempo mudou'),
-	writeln('[1] Pronto para a simulação : '),
+	write('[1] Pronto para a simulação : '),
 	executar().
 
 executar :-
@@ -75,35 +49,63 @@ validarEntrada(X) :-
     X > 1 -> exception().
 
 jogar :- 
-	desenhaTabela(),
+	criaTabuleiro(Tabuleiro),
+	desenhaTabuleiro(Tabuleiro),
 	operar(),
 	sair().
 
-desenhaTabela :- 
-	nl, writeln('   A B C D E F G H I J L M'),
-	writeln('1  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('2  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('3  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('4  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('5  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('6  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('7  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('8  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('9  0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('10 0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('11 0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('12 0 0 0 0 0 0 0 0 0 0 0 0'),
-	writeln('--------------------------'),nl.
-    
+criaTabuleiro([[#,a,b,c,d,e,f,g,h,i,j,k,l], [1,0,0,0,0,0,0,0,0,0,0,0,0], [2,0,0,0,0,0,0,0,0,0,0,0,0], [3,0,0,0,0,0,0,0,0,0,0,0,0],[4,0,0,0,0,0,0,0,0,0,0,0,0], [5,0,0,0,0,0,0,0,0,0,0,0,0], [6,0,0,0,0,0,0,0,0,0,0,0,0], [7,0,0,0,0,0,0,0,0,0,0,0,0], [8,0,0,0,0,0,0,0,0,0,0,0,0], [9,0,0,0,0,0,0,0,0,0,0,0,0], [10,0,0,0,0,0,0,0,0,0,0,0,0], [11,0,0,0,0,0,0,0,0,0,0,0,0], [12,0,0,0,0,0,0,0,0,0,0,0,0]]).
+
+desenhaTabuleiro([Z,A,B,C,D,E,F,G,H,I,J,K,L]) :-  
+    nl,  
+    desenhaLinha(Z),
+    desenhaLinha(A),    
+    desenhaLinha(B),
+    desenhaLinha(C),
+    desenhaLinha(D),
+    desenhaLinha(E),
+    desenhaLinha(F),
+    desenhaLinha(G),
+    desenhaLinha(H),
+    desenhaLinha(I),
+    desenhaLinha(J),
+    desenhaLinha(K),
+    desenhaLinha(L),
+    nl.  
+
+desenhaLinha([Z,A,B,C,D,E,F,G,H,I,J,K,L]) :-  
+    print(Z),
+    print('   '),
+    print(A),  
+    print('   '),
+    print(B),  
+    print('   '),
+    print(C),  
+    print('   '),
+    print(D),  
+    print('   '),
+    print(E),  
+    print('   '),
+    print(F),  
+    print('   '),
+    print(G),  
+    print('   '),
+    print(H),  
+    print('   '),
+    print(I),  
+    print('   '),
+    print(J),  
+    print('   '),
+    print(K),  
+    print('   '),
+    print(L),nl.
+
 operar() :-    
     read_line_to_codes(user_input, Y3),
-	string_to_atom(Y3, Coluna), 
+	string_to_atom(Y3, Y), 
     read_line_to_codes(user_input, Z3),
 	string_to_atom(Z3,Z2),
-	atom_number(Z2,Linha),	
-	X -> posicao(Coluna,Linha),
-    write(X).
-    
+	atom_number(Z2,Z).    
 	
 exception :-
 	derrota(),
---
+++ versaoSegurança.pl
@@ -0,0 +1,114 @@
+:- initialization (main).
+
+
+
+posicao(a, 1, virus(X, Resposta)). posicao(a, 2, virus(X, Resposta)). posicao(a, 3, virus(X, Resposta)). posicao(a, 4, virus(X, Resposta)). posicao(a, 5, virus(X, Resposta)). posicao(a, 6, virus(X, Resposta)). posicao(a, 7, virus(X, Resposta)). posicao(a, 8, virus(X, Resposta)). posicao(a, 9, virus(X, Resposta)). posicao(a, 10, virus(X, Resposta)). posicao(a, 11, virus(X, Resposta)). posicao(a, 12, virus(X, Resposta)). 
+
+posicao(b, 1, virus(X, Resposta)). posicao(b, 2, virus(X, Resposta)). posicao(b, 3, virus(X, Resposta)). posicao(b, 4, virus(X, Resposta)). posicao(b, 5, virus(X, Resposta)). posicao(b, 6, virus(X, Resposta)). posicao(b, 7, virus(X, Resposta)). posicao(b, 8, virus(X, Resposta)). posicao(b, 9, virus(X, Resposta)). posicao(b, 10, virus(X, Resposta)). posicao(b, 11, virus(X, Resposta)). posicao(b, 12,virus(X, Resposta)).
+ 
+posicao(c, 1, virus(X, Resposta)). posicao(c, 2, virus(X, Resposta)). posicao(c, 3, virus(X, Resposta)). posicao(c, 4, virus(X, Resposta)). posicao(c, 5, virus(X, Resposta)). posicao(c, 6, virus(X, Resposta)). posicao(c, 7, virus(X, Resposta)). posicao(c, 8, virus(X, Resposta)). posicao(c, 9, virus(X, Resposta)). posicao(c, 10, virus(X, Resposta)). posicao(c, 11, virus(X, Resposta)). posicao(c, 12, virus(X, Resposta)).
+
+posicao(d, 1, virus(X, Resposta)). posicao(d, 2, virus(X, Resposta)). posicao(d, 3, virus(X, Resposta)). posicao(d, 4, virus(X, Resposta)). posicao(d, 5, virus(X, Resposta)). posicao(d, 6, virus(X, Resposta)). posicao(d, 7, virus(X, Resposta)). posicao(d, 8, virus(X, Resposta)). posicao(d, 9, virus(X, Resposta)). posicao(d, 10, virus(X, Resposta)).posicao(d, 11, virus(X, Resposta)). posicao(d, 12, virus(X, Resposta)).
+
+posicao(e, 1, virus(X, Resposta)). posicao(e, 2, virus(X, Resposta)). posicao(e, 3, virus(X, Resposta)). posicao(e, 4, virus(X, Resposta)). posicao(e, 5, virus(X, Resposta)). posicao(e, 6, virus(X, Resposta)). posicao(e, 7, virus(X, Resposta)). posicao(e, 8, virus(X, Resposta)). posicao(e, 9, virus(X, Resposta)). posicao(e, 10, virus(X, Resposta)). posicao(e, 11, virus(X, Resposta)). posicao(e, 12, virus(X, Resposta)). 
+
+posicao(f, 1, virus(X, Resposta)). posicao(f, 2, virus(X, Resposta)). posicao(f, 3, virus(X, Resposta)). posicao(f, 4, virus(X, Resposta)). posicao(f, 5, virus(X, Resposta)). posicao(f, 6, virus(X, Resposta)). posicao(f, 7, virus(X, Resposta)). posicao(f, 8, virus(X, Resposta)). posicao(f, 9, virus(X, Resposta)). posicao(f, 10, virus(X, Resposta)). posicao(f, 11, virus(X, Resposta)). posicao(f, 12, virus(X, Resposta)).
+
+posicao(g, 1, virus(X, Resposta)). posicao(g, 2, virus(X, Resposta)). posicao(g, 3, virus(X, Resposta)). posicao(g, 4, virus(X, Resposta)). posicao(g, 5, virus(X, Resposta)). posicao(g, 6, virus(X, Resposta)). posicao(g, 7, virus(X, Resposta)). posicao(g, 8, virus(X, Resposta)). posicao(g, 9, virus(X, Resposta)). posicao(g, 10, virus(X, Resposta)). posicao(g, 11, virus(X, Resposta)). posicao(g, 12, virus(X, Resposta)).
+
+posicao(h, 1, virus(X, Resposta)). posicao(h, 2, virus(X, Resposta)). posicao(h, 3, virus(X, Resposta)). posicao(h, 4, virus(X, Resposta)). posicao(h, 5, virus(X, Resposta)). posicao(h, 6, virus(X, Resposta)). posicao(h, 7, virus(X, Resposta)). posicao(h, 8, virus(X, Resposta)). posicao(h, 9, virus(X, Resposta)). posicao(h, 10, virus(X, Resposta)). posicao(h, 11, virus(X, Resposta)). posicao(h, 12, virus(X, Resposta)). 
+
+posicao(i, 1, virus(X, Resposta)). posicao(i, 2, virus(X, Resposta)). posicao(i, 3, virus(X, Resposta)). posicao(i, 4, virus(X, Resposta)). posicao(i, 5, virus(X, Resposta)). posicao(i, 6, virus(X, Resposta)). posicao(i, 7, virus(X, Resposta)). posicao(i, 8, virus(X, Resposta)). posicao(i, 9, virus(X, Resposta)). posicao(i, 10, virus(X, Resposta)). posicao(i, 11, virus(X, Resposta)). posicao(i, 12, virus(X, Resposta)).
+
+posicao(j, 1, virus(X, Resposta)). posicao(j, 2, virus(X, Resposta)). posicao(j, 3, virus(X, Resposta)). posicao(j, 4, virus(X, Resposta)). posicao(j, 5, virus(X, Resposta)). posicao(j, 6,virus(X, Resposta)). posicao(j, 7, virus(X, Resposta)). posicao(j, 8, virus(X, Resposta)). posicao(j, 9, virus(X, Resposta)). posicao(j, 10, virus(X, Resposta)). posicao(j, 11, virus(X, Resposta)). posicao(j, 12, virus(X, Resposta)).
+
+posicao(l, 1, virus(X, Resposta)). posicao(l, 2, virus(X, Resposta)). posicao(l, 3, virus(X, Resposta)). posicao(l, 4, virus(X, Resposta)). posicao(l, 5, virus(X, Resposta)). posicao(l, 6, virus(X, Resposta)). posicao(l, 7, virus(X, Resposta)). posicao(l, 8, virus(X, Resposta)). posicao(l, 9, virus(X, Resposta)). posicao(l, 10, virus(X, Resposta)). posicao(l, 11, virus(X, Resposta)). posicao(l, 12, virus(X, Resposta)).
+
+posicao(m, 1, virus(X, Resposta)). posicao(m, 2, virus(X, Resposta)). posicao(m, 3, virus(X, Resposta)). posicao(m, 4, virus(X, Resposta)). posicao(m, 5, virus(X, Resposta)). posicao(m, 6, virus(X, Resposta)). posicao(m, 7, virus(X, Resposta)). posicao(m, 8, virus(X, Resposta)). posicao(m, 9, virus(X, Resposta)). posicao(m, 10, virus(X, Resposta)). posicao(m, 11, virus(X, Resposta)). posicao(m, 12, virus(X, Resposta)).
+ 
+virus(X, R) :- 
+	X =:= 1 -> R is 1;
+	X =:= 2 -> R is 2;
+	X =:= 3 -> R is 3;
+	X =:= 4 -> R is 4.
+
+main :-
+	nl, write('      ###########      bombardment of the virus      ###########'),nl,
+	writeln('      -----------------------------------------------------------'),
+	resumo(),
+	iniciar().	
+	
+resumo :-
+	writeln('   No ano de 2046, um grupo de cientista conseguem terminar o maior feito'),
+ 	writeln('da humanidade “um botão de reiniciar” uma máquina do tempo. Motivo para'),
+	writeln('ser comemorado pois a única salvação da terra é voltar no tempo e destruir'),
+	writeln('todas as bases que contém o vírus que acabou com quase toda a população do'), 
+	writeln('planeta. Irineu foi o soldado qualificado para essa missão , voltando para'),
+	writeln('o ano de 2014, após 5 anos preso nas instalações secretas da CIA conseguiu'),
+	writeln('convencer o diretor da CIA alertando de diversos desastres antes mesmo de ter'), 
+	writeln('acontecido. Só que a arma biológica irá ser usada no ano de 2019 no dia 5 de'),
+	writeln('março e faltando apenas um dia para impedir esses ataques, vão bombardear pontos'),
+	writeln('exatos para a destruição dessas instalações e assim salvando a humanidade.'),
+	writeln('Ajude Irineu a Salvar o Mundo !!!').
+
+
+iniciar	 :-
+	nl,writeln('    Bem Vindo ao ano começo do fim , a gasolina está acabando e o mundo também'),
+	writeln('só você poderá salva-lo e deter o virus mortal! Baseado em uma historia real.'),
+	writeln('Se voce nao lembra foi por que o mundo ja foi salvo e sua linha do tempo mudou'),
+	writeln('[1] Pronto para a simulação : '),
+	executar().
+
+executar :-
+    repeat,
+    read_line_to_codes(user_input, X3),
+    string_to_atom(X3,X2),
+    atom_number(X2,X),
+    validarEntrada(X).
+    
+derrota :-
+	nl, writeln('--------------- ######  Derrota ###### -------------------'),
+	writeln('Voce nao foi capaz de destruir as bases inimigas a tempo.'),nl,
+	writeln('                    FIM DO MUNDO !!'),
+	writeln('----------------------------------------------------------').
+
+validarEntrada(X) :-    
+    X =:= 1 -> jogar();
+    X < 1 -> exception();
+    X > 1 -> exception().
+
+jogar :- 
+	desenhaTabela(),
+	operar(),
+	sair().
+
+desenhaTabela :- 
+	
+	nl, writeln('   A B C D E F G H I J L M'),
+	writeln('1  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('2  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('3  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('4  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('5  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('6  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('7  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('8  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('9  0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('10 0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('11 0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('12 0 0 0 0 0 0 0 0 0 0 0 0'),
+	writeln('--------------------------'),nl.
+    
+operar() :-    
+    read_line_to_codes(user_input, Y3),
+	string_to_atom(Y3, Coluna), 
+    read_line_to_codes(user_input, Z3),
+	string_to_atom(Z3,Z2),
+	atom_number(Z2,Linha),	
+	write(posicao(Coluna,Linha, 1)).    
+	
+exception :-
+	derrota(),
+	sair().
+
+sair() :- halt(0).
